be8ac330fbe570443cfbd11b8364481020dcf174
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2018-11-19 00:10:03 +0700

    Add SplashScreen

---
+++ app/src/main/java/id/ariefpurnamamuharram/skinlesionclassificationbyai/splash/SplashActivity.kt
@@ -0,0 +1,40 @@
+package id.ariefpurnamamuharram.skinlesionclassificationbyai.splash
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.startActivity
+import id.ariefpurnamamuharram.skinlesionclassificationbyai.R
+import id.ariefpurnamamuharram.skinlesionclassificationbyai.MainActivity
+
+class SplashActivity: AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+    private val splashDelay: Long = 3000 // 3 seconds
+
+    private val mRunnable: Runnable = Runnable {
+        if(!isFinishing) {
+            startActivity<MainActivity>()
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Initialize the handler
+        mDelayHandler = Handler()
+
+        // Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, splashDelay)
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+}
